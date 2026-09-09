f5bbc944948be935790cec93be82fedfa2a61c36
thanosfi1 <thanosfotis3@gmail.com>
2026-09-09 07:47:35 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('push', function(event) {
+  if (!event.data) return;
+  const data = event.data.json();
+  const options = {
+    body: data.body,
+    icon: '/icon.png',
+    badge: '/icon.png'
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
