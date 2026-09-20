54f491f68e66176bc501f7a5395d4e70cf201985
thanosfi1 <thanosfotis3@gmail.com>
2026-09-20 15:45:05 +0300

    Create APLANUS Sky v0.1 foundation

---
+++ js/sky/sensors.js
@@ -0,0 +1,4 @@
+// APLANUS Sky sensor layer.
+// Θα ενεργοποιηθεί στο επόμενο στάδιο για compass/gyroscope και AR.
+// Παραμένει σκόπιμα απομονωμένο ώστε το Sky v0.1 να μη βασίζεται σε sensors.
+export const sensorState={supported:false,heading:null,pitch:null,roll:null};
